d904838f7ad0d4693a196c1fd1fdcb18299667ab
ZanderKoch <zander.koch02@gmail.com>
2025-01-22 13:35:49 +0100

    Add negative number support

--- src/main/java/zander/lexicon/calculator/Tokenizer.java
+++ src/main/java/zander/lexicon/calculator/Tokenizer.java
@@ -22,13 +22,22 @@ public class Tokenizer {
         while (!remainingInput.isEmpty()) {
             boolean matched = false;
 
-
+            System.out.println(remainingInput);
             for (TokenType tokenType : TokenType.values()) {
                 Matcher matcher = tokenType.getPattern().matcher(remainingInput);
                 if (matcher.lookingAt()) {
                     String matchedText = matcher.group();
+                    //special handling for numbers
                     if (tokenType == TokenType.NUMBER) {
-                        tokens.add(new Token(tokenType, Double.parseDouble(matchedText.trim())));
+                        if (negativeNumberEligible(tokens)){
+                            tokens.remove(tokens.size() - 1);
+                            tokens.add(new Token(tokenType, Double.parseDouble(matchedText.trim()) * -1));
+                        } else {
+                            tokens.add(new Token(tokenType, Double.parseDouble(matchedText.trim())));
+                        }
+
+
+                    //all other cases
                     } else {
                         tokens.add(new Token(tokenType, matchedText.trim()));
                     }
@@ -40,5 +49,21 @@ public class Tokenizer {
         }
 
         return tokens;
+    }
+
+    private boolean negativeNumberEligible(List<Token> tokenList) {
+        if (tokenList.size() == 1 && tokenList.getFirst().getType() == TokenType.MINUS) {
+            return true;
+        }
+
+        if (tokenList.size() >= 2) {
+            Token lastToken = tokenList.getLast();
+            Token secondLastToken = tokenList.get(tokenList.size() - 2);
+            if (lastToken.getType() == TokenType.MINUS &&
+                    !secondLastToken.matchTypes(TokenType.NUMBER, TokenType.RIGHT_PAREN)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
